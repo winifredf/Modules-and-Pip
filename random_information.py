ff1576d6bf14944df10d3f8f53ac916ce15e7c77
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-17 18:51:55 -0400

    created random information file.

---
+++ random_information.py
@@ -0,0 +1,10 @@
+destinys_child = ["Beyonce", "Michelle", "Kelly"]
+healthy_greens = ["spinach", "kale", "collard greens", "broccoli", "cabbage", "asparagus"]
+number_of_children = 3
+most_important = "God"
+
+def get_file_ext(filenmae):
+    return filename[filename.index(".") + 1:]
+
+def play_dominos(num):
+    return random.randint(0, num)
